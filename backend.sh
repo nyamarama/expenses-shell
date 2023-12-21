de496230e7e76006e3b11553e154143f62169dd3
nyamarama <blsdregis@gmail.com>
2023-12-21 14:32:17 -0600

    commit

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 
-cp backend.sh /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 
