65823b1e04f8d3a1c448d2ca55359ad750b53120
nyamarama <blsdregis@gmail.com>
2023-12-26 14:49:18 -0600

    commit

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,39 @@
-echo -e "\e[32m installing nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+log_file=/tmp/expense.log
+color="\e[32m"
 
-echo -e "\e[34m copy the configuration file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo success
+  else
+    echo failure
+  fi
+}
 
-echo -e "\e[32m remove the default content \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
-echo -e "\e[34m download the frontend content \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo -e "${color} installing nginx \e[0m"
+dnf install nginx -y &>>$log_file
+status_check
 
-echo -e "\e[32m change directory and unzip the content \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${color} copy the configuration file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+status_check
 
-echo -e "\e[34m enable and restart nginx \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+echo -e "${color} remove the default content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+status_check
+
+echo -e "${color} download the frontend content \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+status_check
+
+echo -e "${color} change directory and unzip the content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+status_check
+
+echo -e "${color} enable and restart nginx \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+status_check
 
 
